7e7a417a3e33d4bda1d7822745ac6f5d94acd2c5
hendpraz <mhendryp99@gmail.com>
2019-11-13 11:02:14 +0700

    Add Request-Response Logic For POST Request

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var transactionRouter = require('./routes/transaction');
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/transaction', transactionRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,6 +33,8 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  bodyParser.urlencoded({ extended: true });
+  bodyParser.json();
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
