20aae764f6349972ff02e07ba7eafd5228a30b34
hendpraz <mhendryp99@gmail.com>
2019-11-17 16:14:34 +0700

    Re-update Deployment Bash

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,7 +16,7 @@ sudo chmod 600 ~/.ssh/id_rsa
 # ** End of alternative approach
 
 # disable the host key checking.
-./deploy/disableHostKeyChecking.sh
+sudo ./deploy/disableHostKeyChecking.sh
 
 # we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
 # comma seperated values of ip addresses.
@@ -27,12 +27,12 @@ DEPLOY_SERVERS=$DEPLOY_SERVERS
 # ${string//substring/replacement}
 # our substring is "," and we replace it with nothing.
 ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
-echo "ALL_SERVERS ${ALL_SERVERS}"
+sudo echo "ALL_SERVERS ${ALL_SERVERS}"
 
 # Lets iterate over this array and ssh into each EC2 instance
 # Once inside the server, run updateAndRestart.sh
 for server in "${ALL_SERVERS[@]}"
 do
-  echo "deploying to ${server}"
-  ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
+  sudo echo "deploying to ${server}"
+  sudo ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
 done
