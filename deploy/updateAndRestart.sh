5928e711ff5e3886c75ae79f66248d03edb835df
hendpraz <mhendryp99@gmail.com>
2019-11-17 13:54:42 +0700

    Update Deployment Bash

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -11,7 +11,7 @@ git clone https://gitlab.informatika.org/if3110-2019-02-k03-18/ws-transaksi.git
 
 #source the nvm file. In an non
 #If you are not using nvm, add the actual path like
-# PATH=/home/ubuntu/node/bin:$PATH
+PATH=/home/ubuntu/node/bin:$PATH
 source /home/ubuntu/.nvm/nvm.sh
 
 # stop the previous pm2
