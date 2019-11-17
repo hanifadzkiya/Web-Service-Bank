c2cf7f99d8a7a4f63522e6d32ba6515e59b6f6f7
hendpraz <mhendryp99@gmail.com>
2019-11-17 16:42:23 +0700

    Re-update Deployment Bash

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,10 +8,10 @@ set -e
 #echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 # ** Alternative approach
-sudo rm -rf ~/.ssh
-sudo mkdir ~/.ssh
+# sudo rm -rf ~/.ssh
+sudo mkdir -p ~/.ssh
 sudo chmod 777 ~/.ssh
-sudo echo -e "$PRIVATE_KEY" > ~/.ssh/id_rsa
+sudo echo -e "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCCE0STvqg2CdcoRcbkbmZvWlfoyhPMxMdx08ugZqegLEsWI8qUN8fnaTBvj4vJh3Paee/fgfd3vXlYhm6e6gBT8ftuWcExgNVdOkrWxxh64vOFIPCvJyp6yQdjjS6DfWzDTBrUjpUb+cr56LMYtrUEGknlukZHT3qJRy+QoNYA2876+/ngkXO05QcJcVBVQ1F6Ul+QBs5dZNn5EmsTdtsL02GYF/oU3NNxb2CL1Bqqd4wI+ebPJJ59X97a28It1b72HBOdwjAEfddYlIYb3l5rmete/CtZIRNnQFNL+mLD6g/GG4h20pkH8xEnnA2+DJMYNrmeA7z9+T88Ldoc5yRN" > ~/.ssh/id_rsa
 sudo chmod 600 ~/.ssh/id_rsa
 # ** End of alternative approach
 
@@ -25,7 +25,8 @@ sudo chmod 600 ~/.ssh/id_rsa
 sudo set -e
 # mkdir -p ~/.ssh
 sudo touch ~/.ssh/config
-sudo echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
+sudo bash -c 'echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config'
+#sudo echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
 
 # we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
 # comma seperated values of ip addresses.
@@ -35,13 +36,13 @@ DEPLOY_SERVERS=$DEPLOY_SERVERS
 # In UNIX, we can use this commond to do this
 # ${string//substring/replacement}
 # our substring is "," and we replace it with nothing.
-ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
-sudo echo "ALL_SERVERS ${ALL_SERVERS}"
-
-# Lets iterate over this array and ssh into each EC2 instance
-# Once inside the server, run updateAndRestart.sh
-for server in "${ALL_SERVERS[@]}"
-do
-  sudo echo "deploying to ${server}"
-  sudo ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
-done
+#ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
+#sudo echo "ALL_SERVERS ${ALL_SERVERS}"
+#
+## Lets iterate over this array and ssh into each EC2 instance
+## Once inside the server, run updateAndRestart.sh
+#for server in "${ALL_SERVERS[@]}"
+#do
+sudo echo "deploying to 52.220.86.67"
+sudo ssh ubuntu@52.220.86.67 'bash' < ./deploy/updateAndRestart.sh
+#done
