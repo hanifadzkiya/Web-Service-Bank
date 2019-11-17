b4762ff808d8d2b7cfca7d5f678ce6dbae2a50a4
hendpraz <mhendryp99@gmail.com>
2019-11-17 16:16:00 +0700

    Re-update Deployment Bash

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,7 +16,16 @@ sudo chmod 600 ~/.ssh/id_rsa
 # ** End of alternative approach
 
 # disable the host key checking.
-sudo ./deploy/disableHostKeyChecking.sh
+# sudo ./deploy/disableHostKeyChecking.sh
+
+Host *
+  StrictHostKeyChecking no
+
+# any future command that fails will exit the script
+sudo set -e
+# mkdir -p ~/.ssh
+sudo touch ~/.ssh/config
+sudo echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
 
 # we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
 # comma seperated values of ip addresses.
