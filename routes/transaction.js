5f24f6a1cdab3838378dd290830ba97b44aea958
hanif adzkiya <hanifadzkiya@gmail.com>
2019-11-18 15:39:10 +0700

    Linter

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,35 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-'use strict';
+"use strict";
 
-var response = require('../util/response');
-var connection = require('../util/dbconnection');
+var response = require("../util/response");
+var connection = require("../util/dbconnection");
 
 /*
 Router to get transaction details given by transaction id
 */
-router.get('/', function(req, res) {
+router.get("/", function(req, res) {
     if(req.query.id){
         let id = req.query.id;
         connection.query("SELECT * from transaksi WHERE id = " + id, 
             function (error, rows){
                 if(error){
                     console.log(error);
-            } else {
+                } else {
                     response.ok(rows,"Success Get Transaction with id = " + id,res);
-            }
-        });
+                }
+            });
     } else if(req.query.id_user){
         let id_user = req.query.id_user;
         connection.query("SELECT * from transaksi WHERE id_user = " + id_user, 
             function (error, rows){
                 if(error){
                     console.log(error);
-            } else {
+                } else {
                     response.ok(rows,"Success Get Transaction with id_user = " + id_user,res);
-            }
-        });
+                }
+            });
     }
 });
 
