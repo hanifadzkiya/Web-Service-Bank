a6bbd6e9519314e6666eba9b36510fd055902546
hendpraz <mhendryp99@gmail.com>
2019-11-20 20:25:28 +0700

    Update yml file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,7 +13,7 @@ echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 #
 sudo mkdir -p ~/.ssh
 sudo touch ~/.ssh/config
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
+sudo bash -c 'echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config'
 
 # we have already setup the DEPLOY_SERVER in our gitlab settings
 DEPLOY_SERVER=$DEPLOY_SERVER
